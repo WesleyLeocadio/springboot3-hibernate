6499b048453097b517eafce454171011ee457efd
Wesley Leocadio <silvawesley374@gmail.com>
2023-08-22 13:32:55 -0300

    Entidade Pedido, Datas com Instant e padrão ISO 8601 - PARTE 1

--- src/main/java/com/wesleydev/course/entities/User.java
+++ src/main/java/com/wesleydev/course/entities/User.java
@@ -4,20 +4,31 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     private String name;
+    
     private String email;
+    
     private String phone;
+    
     private String password;
+    
+    @OneToMany(mappedBy = "client")
+    private List<Order> Orders = new ArrayList<>();
 
     public User() {
     }
@@ -70,7 +81,11 @@ public class User implements Serializable {
     public void setPassword(String password) {
         this.password = password;
     }
-
+    
+    public List<Order> getOrders() {
+		return Orders;
+	}
+    
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -95,4 +110,5 @@ public class User implements Serializable {
             return false;
         return true;
     }
+
 }
