3a611ce45e967c18ac1f69af40137191ef3350f1
Wesley Leocadio <silvawesley374@gmail.com>
2023-08-29 16:19:04 -0300

    Entidade pedido. Datas com Instant e padrão ISO8601 -PARTE 2

--- src/main/java/com/wesleydev/course/entities/Order.java
+++ src/main/java/com/wesleydev/course/entities/Order.java
@@ -1,8 +1,8 @@
 package com.wesleydev.course.entities;
 
+
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -20,20 +20,20 @@ public class Order implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
 	private Instant moment;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-	
-	public Order() {}
 
-	public Order(Long id, Instant moment, User cliente) {
+	public Order() {
+	}
+
+	public Order(Long id, Instant moment, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
-		this.client = cliente;
+		this.client = client;
 	}
 
 	public Long getId() {
@@ -62,7 +62,10 @@ public class Order implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -74,7 +77,11 @@ public class Order implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Order other = (Order) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-
 }
