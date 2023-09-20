524b7c41f147cce02a48328ca54795f7bf1fbef1
Wesley Leocadio <silvawesley374@gmail.com>
2023-09-20 16:18:36 -0300

    Tratamento de exceção - findById

--- src/main/java/com/wesleydev/course/services/UserService.java
+++ src/main/java/com/wesleydev/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.wesleydev.course.entities.User;
 import com.wesleydev.course.repositories.UserRepository;
+import com.wesleydev.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,7 +21,8 @@ public class UserService {
 	}
 
 	public User findById(Long id) {
-		return repository.findById(id).get();
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
